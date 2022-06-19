36367fc7fc38f52229aa107d527743fb55d93455
DarxyDev <damongarvin@gmail.com>
2022-06-19 16:53:07 -0400

    basic functions working. need to add user options

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
 createSketchBox();
-addPixelBorder();
+//addPixelBorder();
--- sketch_mechanics.js
+++ sketch_mechanics.js
@@ -4,18 +4,36 @@ let sketchSizeX = 10;
 let sketchSizeY = 10;
 let sketchSize = sketchSizeX * sketchSizeY;
 let pixelElements = new Array(sketchSize);
-let pixelColor = 'black';
 
-//functions
-function setBackground(e){
-    e.target.style.backgroundColor = pixelColor;    
+//User selected variables
+let pixelColor = 'black';
+let adjustForRoundingError = false;
+
+//event functions
+document.addEventListener('mousedown',setMouseDown);
+let mouseDown = false;
+function setMouseDown(e){
+    mouseDown = true;
 }
+document.addEventListener('mouseup', removeMouseDown);
+function removeMouseDown(e){
+    mouseDown = false;
+}
+function setBackgroundHover(e){
+    if(mouseDown)e.target.style.backgroundColor = pixelColor;  
+}
+function setBackgroundClick(e){
+    e.target.style.backgroundColor = pixelColor;
+    e.preventDefault(); //Reminder: This MIGHT cause issues. It did on setBackgroundHover
+}
+//options functions
 function addPixelBorder(){
     pixelElements.forEach(element => element.classList.add('pixelBorder'));
 }
 function removePixelBorder(){
     pixelElements.forEach(element => element.classList.remove('pixelBorder'));
 }
+//constructor functions
 function pxToVh(px){
     let documentHeight = document.documentElement.offsetHeight || document.documentElement.clientHeight;
     return px / documentHeight * 100;
@@ -29,13 +47,16 @@ function setPixelAttributes(element, index){
     //set size
     let percentSizeX = 100 / sketchSizeX;
     let percentSizeY = 100 / sketchSizeY;
-    element.style.width = `${percentSizeX}%`;
-    element.style.height = `${percentSizeY}%`;
+    let roundingAdjust = 1;
+    if(adjustForRoundingError) roundingAdjust = 1.03;
+    element.style.width = `${percentSizeX * roundingAdjust}%`;
+    element.style.height = `${percentSizeY * roundingAdjust}%`;
     //set position
     element.style.left = `${index % sketchSizeX * percentSizeX}%`;
     element.style.top = `${Math.floor(index / sketchSizeX)* percentSizeY}%`;
-    //
-    element.addEventListener('click',setBackground);
+    //add event listeners
+    element.addEventListener('mouseover',setBackgroundHover);
+    element.addEventListener('mousedown',setBackgroundClick);
 }
 function createPixelElement(index = 0){
     let pElement = document.createElement('div');
